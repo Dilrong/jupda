31fec822f0586163d8001f80cdef0b4a4ee4367a
dilrong <wet932@naver.com>
2021-06-23 20:41:25 +0900

    [feat] logo&navigation

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,16 +1,25 @@
-import React from 'react'
-import { Link } from 'gatsby'
-import styles from './navigation.module.css'
+import React from "react";
+import { Link } from "gatsby";
+import styles from "./navigation.module.css";
 
 export default () => (
-  <nav role="navigation">
-    <ul className={styles.navigation}>
-      <li className={styles.navigationItem}>
-        <Link to="/">Home</Link>
-      </li>
-      <li className={styles.navigationItem}>
-        <Link to="/blog/">Blog</Link>
-      </li>
-    </ul>
-  </nav>
-)
+  <div className={styles.navigationWrap}>
+    <div className={styles.navigationLogo}>줍</div>
+    <nav role="navigation">
+      <ul className={styles.navigation}>
+        <li className={styles.navigationItem}>
+          <Link to="/Events">Event</Link>
+        </li>
+        <li className={styles.navigationItem}>
+          <Link to="/airdrops/">AirDrop</Link>
+        </li>
+        <li className={styles.navigationItem}>
+          <Link to="/Whitelist/">Whitelist</Link>
+        </li>
+        <li className={styles.navigationItem}>
+          <Link to="/referral/">Referral</Link>
+        </li>
+      </ul>
+    </nav>
+  </div>
+);
